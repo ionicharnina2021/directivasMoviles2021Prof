ad666a27937d0cd9e38d6183163af62919a487f1
programacion <>
2020-10-23 13:28:25 +0200

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { GeneradorMoviles } from './core/control/generadorMoviles';
+import { Movil } from './core/model/movil';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directivasMoviles2021Prof';
+  private listaMoviles:Array<Movil>;
+  constructor(){
+    this.listaMoviles=new GeneradorMoviles().movilesList;
+  }
 }
